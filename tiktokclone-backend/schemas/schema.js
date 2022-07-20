3683be53712fe369dd937cabb5da998c954a37cb
Andrew Midthun <midth002@gmail.com>
2022-07-20 12:01:00 -0500

    added schematypes.

--- tiktokclone-backend/schemas/schema.js
+++ tiktokclone-backend/schemas/schema.js
@@ -15,6 +15,7 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    /* Your types here! */
+    
+    post, comment, user, postedBy
   ]),
 })
